15471408fc6f424e4f978576c48c16a6b58f8ab9
Metin <126709211+Pyro137@users.noreply.github.com>
2025-04-04 13:59:05 +0300

    website title changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Metin Işıkcan',
+  description: 'Portfolio Website',
   generator: 'v0.dev',
 }
 
